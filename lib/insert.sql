c5caea87e87ec7dad090f58d74195887e035077f
Ian Mayton <ian.mayton@gmail.com>
2020-08-11 19:57:47 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,6 +1,6 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 5, 'Male', 'Black', 'Sweet', TRUE);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 3, 'Female', 'Brown', 'Mean', TRUE);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 7, 'Female', 'Brown', 'Sweet', TRUE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 5, 'Male', 'Black', 'Sweet', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 3, 'Female', 'Brown', 'Mean', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 7, 'Female', 'Brown', 'Sweet', 1);
 INSERT INTO bears (name) VALUES ('Sergeant Brown');
 INSERT INTO bears (name) VALUES ('Melissa');
 INSERT INTO bears (name) VALUES ('Grinch');
